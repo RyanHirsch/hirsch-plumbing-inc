2c6ff8709eac33cdf3589766a22e2eceb626b6d9
Ryan Hirsch <ryan.hirsch@gmail.com>
2018-11-18 21:09:26 -0600

    update the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,59 @@
 import React from "react";
-import { Link } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+import styled from "styled-components";
 
 import Layout from "../components/layout";
 
-const AboutPage = () => (
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* align-items: center; */
+
+  .about__image {
+    max-width: 300px;
+    height: 399px;
+  }
+`;
+
+const About = ({ image }) => (
   <Layout>
     <h1>About</h1>
-    <Link to="/">Go back to the homepage</Link>
+    <Wrapper>
+      <Img className="about__image" fluid={image} />
+      <p>
+        Hirsch Plumbing is a locally owned and operated company providing
+        comprehensive repair, replacement, and installation services in Monroe
+        County and surrounding areas. By focusing on the principles of
+        professionalism, dependability, and integrity; we’re committed to
+        delivering superior value to each of our clients. At Hirsch Plumbing, we
+        know that our reputation is built on the satisfaction of our customers.
+        That's why you can expect the highest quality of work; outstanding
+        customer service; and honest, affordable pricing. Contact us today to
+        see the difference for yourself. Call to get started or to learn more
+        today.
+      </p>
+    </Wrapper>
   </Layout>
+);
+
+const AboutPage = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "nathan-and-erin.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 300) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <About image={data.placeholderImage.childImageSharp.fluid} />
+    )}
+  />
 );
 
 export default AboutPage;
